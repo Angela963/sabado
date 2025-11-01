c8776b9be919be9e405ca0f4336f547b70b06218
Angela963 <angela.ruvalcaba5887@alumnos.udg.mx>
2025-11-01 09:42:26 -0600

    FIX: 25 y 39

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ linter_pkg = ''
 if usar_linter.lower() == 's':
     linter = input("¿? ") or "flake8"
     linter_pkg = linter.split()[0]
-    cmd_linter = input(f"Análisis estático (Defecto: '{linter} .'): ") or f"{linter} ."
+    cmd_linter = input(f"(Defecto: '{linter} .'): ") or f"{linter} ."
     steps_linter = f"""
     - name: Instalar herramienta de análisis estático
       run: pip install {linter_pkg}
@@ -36,7 +36,7 @@ security_pkg = ''
 if usar_security.lower() == 's':
     security = input("¿Herramienta para análisis de seguridad? ") or "bandit"
     security_pkg = security.split()[0]
-    cmd_security = input(f"Análisis de seguridad: ") or f"{security} ."
+    cmd_security = input(f"Comando: ") or f"{security} ."
     steps_security = f"""
     - name: Instalar herramienta de análisis de seguridad
       run: pip install {security_pkg}
