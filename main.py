8bb79574a2f439a490a518c34a604188a59fc7e7
Angela963 <angela.ruvalcaba5887@alumnos.udg.mx>
2025-11-01 09:43:34 -0600

    FIX: borrar

--- main.py
+++ main.py
@@ -4,25 +4,37 @@ print("\nBienvenido al generador de pipelines para GitHub Actions.\n")
 
 # Versión de Python
 language = "python"
-version = input("¿Qué versión mínima de Python quieres usar?") or "3.8"
+version = input(
+  "¿Qué versión mínima de Python quieres usar? (Ejemplo: 3.8): "
+  ) or "3.8"
 
 # TEST AUTOMATIZADOS
-usar_test = input("¿Quieres incluir tests automáticos? (S/n): ") or "s"
+usar_test = input(
+  "¿Quieres incluir tests automáticos? (S/n): "
+  ) or "s"
 steps_test = ''
 if usar_test.lower() == 's':
-    cmd_test = input("¿Comando para ejecutar tus tests? ") or "pytest"
+    cmd_test = input(
+      "¿Comando para ejecutar tus tests? (por defecto: 'pytest'): "
+      ) or "pytest"
     steps_test = f"""
     - name: Ejecutar tests
       run: {cmd_test}"""
 
 # ANALISIS ESTATICO
-usar_linter = input("¿Quieres análisis de código estático? (S/n): ") or "s"
+usar_linter = input(
+  "¿Quieres análisis de código estático? (S/n): "
+  ) or "s"
 steps_linter = ''
 linter_pkg = ''
 if usar_linter.lower() == 's':
-    linter = input("¿? ") or "flake8"
+    linter = input(
+      "¿Qué herramienta usarás para código estático?"
+      ) or "flake8"
     linter_pkg = linter.split()[0]
-    cmd_linter = input(f"(Defecto: '{linter} .'): ") or f"{linter} ."
+    cmd_linter = input(
+      f"Comando para análisis estático (por defecto: '{linter} .'): "
+      ) or f"{linter} ."
     steps_linter = f"""
     - name: Instalar herramienta de análisis estático
       run: pip install {linter_pkg}
@@ -30,13 +42,19 @@ if usar_linter.lower() == 's':
       run: {cmd_linter}"""
 
 # ANALISIS DE SEGURIDAD
-usar_security = input("¿Quieres análisis de seguridad? (S/n): ") or "s"
+usar_security = input(
+  "¿Quieres análisis de seguridad? (S/n): "
+  ) or "s"
 steps_security = ''
 security_pkg = ''
 if usar_security.lower() == 's':
-    security = input("¿Herramienta para análisis de seguridad? ") or "bandit"
+    security = input(
+      "¿Herramienta para análisis de seguridad"
+      ) or "bandit"
     security_pkg = security.split()[0]
-    cmd_security = input(f"Comando: ") or f"{security} ."
+    cmd_security = input(
+      f"Comando para análisis de seguridad (por defecto: '{security} .'): "
+      ) or f"{security} ."
     steps_security = f"""
     - name: Instalar herramienta de análisis de seguridad
       run: pip install {security_pkg}
@@ -74,5 +92,3 @@ jobs:
 
 with open(workflow_file, "w", encoding="utf-8") as f:
     f.write(yml)
-
-print(f"\n¡Listo! Archivo de workflow creado en: {workflow_file}\n")
